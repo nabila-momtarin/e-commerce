5c8b79403d32023332f0f1244a622c1f529c40b5
nabila-momtarin <nabila.momtarin.me@gmail.com>
2026-01-16 23:49:00 +0600

    fixed cmds

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -5,9 +5,9 @@ import { Request, Response } from "express";
 export const createProductController = async (req: Request, res: Response) => {
   try {
     console.log("Entered in PRODUCT CONTROLLER");
-    console.log("\n\n\n\n\n");
+  //  console.log("\n\n\n\n\n");
     console.log(req.body);
-    console.log("\n\n\n\n\n");
+   // console.log("\n\n\n\n\n");
     const newProductC = await addProduct(req.body);
 
     return res.json({
